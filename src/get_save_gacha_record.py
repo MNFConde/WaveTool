9b5e21b8965c2b84ada37c7110de59dac24b0dbe
MNF_Condé <catyrenparadox@outlook.com>
2025-01-15 23:03:04 +0800

    将参数保存在一个类中，并且不会对获取记录部分代码造成过多修改，并且完善了参数的初始化以及修改；简单完善 了获取记录部分的代码，适配了新的参数类并且简单优化了逻辑

--- src/get_save_gacha_record.py
+++ src/get_save_gacha_record.py
@@ -4,7 +4,8 @@ import chardet
 import re
 import requests
 import json
-import settings
+# import settings
+from settings import settings
 from datetime import datetime
 
 new_record_standard_time = None
@@ -93,7 +94,8 @@ def make_post_url(url_api: str, url_para: dict) -> str:
     return url_api + url_para_str[:-1]
 
 def get_url_para_from_log() -> dict:
-    url_para_str = get_post_url(settings.file_path)
+    url_para_str = get_post_url(settings.game_log_path)
+    print(url_para_str)
     url_para_dict = {
         'svr_id'       : re.search(r'(?<=svr_id=).*?(?=&)', url_para_str).group(),
         'player_id'    : re.search(r'(?<=player_id=).*?(?=&)', url_para_str).group(),
@@ -148,15 +150,15 @@ def insert_or_update(gacha_id: int, gacha_records: list) -> int:
     # 从数据库中读取最后一条消息的时间
     
     
-    # 从头到位时间由大到小，减轻比对数量，并且后续切片时不会多出一条
-    if new_record_standard_time:
+    # 从头到尾时间由大到小，减轻比对数量，并且后续切片时不会多出一条
+    if settings.is_gacha_time_init() and gacha_records: # gacha_record 为空时 i 会不存在
         
         # 比对传入的 gacha_records 的时间
         for i in range(len(gacha_records)):
             record = gacha_records[i]
             record_time = datetime.strptime(record['time'], settings.time_format)
             
-            if record_time > new_record_standard_time:
+            if record_time > settings.gacha_time:
                 continue
             else:
                 break
@@ -179,24 +181,24 @@ def get_save_all_type_gacha_records(post_para_dict: dict) -> dict:
     '''
     -> dict[str: int]
     '''
-    get_records_num = 0
-    update_records_num = 0
-    
-    # 在获取记录之前标记，获取记录过程中该值不能更新
-    global new_record_standard_time
-    new_record_standard_time = settings.get_lastest_time()
+    all_get_records_num = 0
+    all_update_records_num = 0
     
     for gacha_id, gacha_name in settings.gacha_type.items():
         # post_para_dict['']
         # 一次请求以及三次重试
+        print("正在读取{}卡池".format(gacha_name))
         for _ in range(4):
             gacha_records: list = []
             try:
                 gacha_records = get_pointed_type_gacha_records(gacha_id, post_para_dict)
-                get_records_num += len(gacha_records)
+                get_records_num = len(gacha_records)
                 break
             except WaveUrlTimeOut as w: # 把异常抛给更上层
-                raise WaveUrlTimeOut(w.message)
+                raise WaveUrlTimeOut(w.message + str({
+                    "get_records_num"    : all_get_records_num + get_records_num,
+                    "update_records_num" : all_update_records_num,
+                }))
             except Exception as e:
                 if _ == '0':
                     print("请求卡池类型【{}】失败，错误信息：{}，开始重试".format(gacha_name, str(e)))
@@ -208,16 +210,30 @@ def get_save_all_type_gacha_records(post_para_dict: dict) -> dict:
             print("重试失败，跳过该类卡池！")
         
         # 顺序导入，减轻后续遍历时的条数
-        update_records_num += insert_or_update(gacha_id, gacha_records)
+        update_records_num = insert_or_update(gacha_id, gacha_records)
+        print("读取{}条".format(get_records_num))
+        print("更新{}条".format(update_records_num))
+        all_update_records_num += update_records_num
+    
+    # 该值只在获取记录后更新
+    settings.renew_gacha_time()
     
     return {
-        "get_records_num"    : get_records_num,
-        "update_records_num" : update_records_num,
+        "get_records_num"    : all_get_records_num,
+        "update_records_num" : all_update_records_num,
     }
 
 def get_save_gacha_main():
-    para = get_url_para_from_log()
-    return get_save_all_type_gacha_records(para)
+    if settings.settings_complete:
+        para = get_url_para_from_log()
+        try:
+            return get_save_all_type_gacha_records(para)
+        except WaveUrlTimeOut as e:
+            return e.message
+        except Exception as e:
+            print(str(e))
+    else:
+        print("请先进行设置！")
 
 if __name__ == "__main__":
     # if not is_admin():
--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,7 @@
 from tinydb import TinyDB, Query
 from datetime import datetime
+import os
+
 
 gacha_db = TinyDB('./gacha_database.json')
 gacha_records = Query()
@@ -26,24 +28,179 @@ gacha_type = {
 
 time_format = r'%Y-%m-%d %H:%M:%S'
 
+init_args = {
+    "game_path"  : "",
+    "data_path"  : './',
+    "log_path"   : './log',
+    "cache_path" : './cache',
+    "database_lastest_record_time" : '1970-01-01 00:00:00',
+    "analysis_lastest_record_time" : '1970-01-01 00:00:00',
+}
 
-def get_lastest_time():
+# 计算文件夹大小
+def get_dir_size(path_str):
+    total = 0
+    with os.scandir(path_str) as dir:
+        for dir_content in dir:
+            if dir_content.is_file():
+                total += dir_content.stat().st_size
+            elif dir_content.is_dir():
+                total += get_dir_size(dir_content.path)
+    return total
+
+class WaveToolArgs():
     '''
-    找到所有表中的最新时间，比较之后输出其中最新的那个
+    一个参数类
+        1. 记录参数，并且保持全局一致
+        2. 作为设置项，可以热更新，并且在更新时向参数json中存入一份
+        3. 初始时若没有进行设置，使用标志来判断是否可以使用某种功能
+    记录同样放入数据库中
+        4. 记录时间，包括数据库中记录的最新时间以及数据分析过的数据中最新的记录的时间
     '''
-    time_tuple = []
-    for gacha_name in gacha_type.values():
-        gacha_table = gacha_db.table(gacha_name)
-        table_lastest_time_record = gacha_table.get(doc_id = len(gacha_table))
-        if table_lastest_time_record:
-            time_tuple.append(
-                datetime.strptime(
-                    table_lastest_time_record['time'], 
+    def __init__(self):
+        self.args_json_path = './args.json'
+        self.gacha_db = TinyDB(self.args_json_path)
+        self.url_api = '''https://gmserver-api.aki-game2.com/gacha/record/query'''
+        self.url_api_2 = '''https://coapi.cn/v1/mc/gacha.php'''
+        self.post_header = {
+            "User-Agent": "Mozilla/5.0",
+            "Content-Type": "application/json",
+        }
+        self.gacha_type = {
+            1: "UP角色池",
+            2: "UP武器池",
+            3: "常驻角色池",
+            4: "常驻武器池",
+            5: "新手池",
+            6: "新手自选池",
+            7: "感恩自选池",
+        }
+
+        self.time_format = r'%Y-%m-%d %H:%M:%S'
+        
+        self.init_args = {
+            "game_path"  : "",
+            "data_path"  : './',
+            "log_path"   : './log',
+            "cache_path" : './cache',
+            "database_lastest_record_time" : '1970-01-01 00:00:00',
+            "analysis_lastest_record_time" : '1970-01-01 00:00:00',
+        }
+        
+        if len(self.gacha_db) == 0:
+            self.gacha_db.insert(init_args)
+        self.set_args_from_database()
+    
+    def set_args_from_database(self):
+        args_dict = self.gacha_db.get(doc_id = len(self.gacha_db))
+        self.set_args(args_dict)
+
+    @property
+    def game_log_path(self) -> str:
+        return self.game_path + '\Wuthering Waves Game\Client\Saved\Logs\Client.log'
+    
+    @property
+    def cache_size(self):
+        if not os.path.exists(self.cache_path):
+            return 0
+        dir_bytes = get_dir_size(self.cache_path)
+        if dir_bytes < 1024:
+            return "{} B({} 字节)".format(dir_bytes, dir_bytes)
+        elif dir_bytes < pow(1024, 2):
+            return "{} KB ({} 字节)".format(dir_bytes // 1024, dir_bytes)
+        else:
+            return "{} KB ({} 字节)".format(dir_bytes // pow(1024, 2), dir_bytes)
+    
+    @property
+    def settings_complete(self):
+        if self.game_path != "":
+            return True
+        return False
+    
+    @property
+    def gacha_time(self):
+        return datetime.strptime(self.database_time)
+    
+    def is_gacha_time_init(self):
+        if self.database_time == self.init_args['database_lastest_record_time']:
+            return True
+        return False
+
+    def renew_gacha_time(self):
+        '''
+        找到所有表中的最新时间，比较之后输出其中最新的那个
+        '''
+        time_tuple = []
+        for gacha_name in self.gacha_type.values():
+            gacha_table = self.gacha_db.table(gacha_name)
+            table_lastest_time_record = gacha_table.get(doc_id = len(gacha_table))
+            if table_lastest_time_record:
+                time_tuple.append(
+                    datetime.strptime(
+                        table_lastest_time_record['time'], 
+                        time_format,
+                    )
+                )
+        if time_tuple:
+            self.database_time = max(time_tuple)
+
+    def set_args(self, args_dict: dict):
+        '''
+        {
+            "game_path": ,
+            "data_path": ,
+            "log_path" : ,
+            "cache_path" : ,
+            "database_lastest_record_time" : '',
+            "analysis_lastest_record_time" : '',
+        }
+        '''
+        while 1:
+            try:
+                self.game_path = args_dict['game_path']
+                self.data_path = args_dict['data_path']
+                self.log_path = args_dict['log_path']
+                self.cache_path = args_dict['cache_path']
+                self.database_time = datetime.strptime(
+                    args_dict['database_lastest_record_time'],
                     time_format,
                 )
-            )
-    
-    if time_tuple:
-        return max(time_tuple)
-    return None
+                self.analysis_time = datetime.strptime(
+                    args_dict['analysis_lastest_record_time'],
+                    time_format,
+                )
+                break
+            except Exception:
+                signal = input("参数损坏或不全，是否重置？[y/n]")
+                if signal == 'y' or signal == 'Y':
+                    self.init_args()
+                    continue
+                else:
+                    print("无法执行")
+                    break
 
+    # 更新设置
+    def fresh_args(self, args_dict: dict):
+        # args_dict 并不一定有所有的参数
+        for args_name, args_value in args_dict.items():
+            self.gacha_db.update(
+                {
+                    args_name: args_value
+                }, 
+                doc_ids=[len(self.gacha_db)], 
+            )
+        self.set_args_from_database()
+    
+    # 重置设置
+    def init_args(self):
+        self.gacha_db.truncate()
+        self.gacha_db.insert(init_args)
+        self.set_args_from_database()
+        self.renew_gacha_time()
+
+
+settings = WaveToolArgs()
+settings.game_path = r"D:\game\鸣潮\Wuthering Waves"
+# settings.init_args()
+# print(settings.database_time)
+# a.set_args_from_database()
