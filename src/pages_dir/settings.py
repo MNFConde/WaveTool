1d05a94f90fdc73edbd9eec6104416dd4317455f
MNF_Condé <catyrenparadox@outlook.com>
2025-03-04 20:15:43 +0800

    修正了路径设置页面的结构，使得提示和修改更加合理，只是还有部分留空欠缺实现

--- src/pages_dir/settings.py
+++ src/pages_dir/settings.py
@@ -8,7 +8,13 @@ root.withdraw()
 # Make folder picker dialog appear on top of other windows
 root.wm_attributes('-topmost', 1)
 
-def path_set(label_name: str, segment_name: str, note: str = '', init_path: str = '') -> str:
+def path_set(
+        label_name: str, 
+        segment_name: str, 
+        note: str = '', 
+        init_path: str = '',
+    ) -> str:
+    
     
     if segment_name not in st.session_state:
         st.session_state[segment_name] = init_path
@@ -39,11 +45,55 @@ def path_set(label_name: str, segment_name: str, note: str = '', init_path: str 
         autocomplete='on',
         key=segment_name + 'input' + 'b',
     )
-    # st.write(st.session_state[segment_name])
+    if choice:
+        st.toast("路径设定成功，值为：\n{}".format(st.session_state[segment_name]))
+        if st.session_state[segment_name] != init_path:
+            return st.session_state[segment_name]
+    
+    return None
+
+def check_game_str(game_path: str) -> bool:
+    return True
+
+def input_path(path_str, check_funtion: callable = lambda i: True):
+    '''
+    path_str: (str|None)
+    '''
+    if path_str and check_funtion(path_str):
+        st.toast("写入成功（占位）")
 
 st.header("设置")
 st.write("")
-path_set('游戏目录', 'game_path', '找到安装目录下的 Wuthering Waves 文件夹', init_path=settings.game_path)
-path_set('数据存放目录', 'data_path', init_path=settings.data_path)
-path_set('日志存放目录', 'log_path', init_path=settings.game_log_path)
-path_set('缓存存放目录', 'cache_path', init_path=settings.cache_path)
+
+game_path_str = path_set(
+    '游戏目录', 
+    'game_path', 
+    '找到安装目录下的 Wuthering Waves 文件夹', 
+    init_path=settings.game_path
+)
+
+input_path(game_path_str)
+
+data_path_str = path_set(
+    '数据存放目录', 
+    'data_path', 
+    init_path=settings.data_path
+)
+
+input_path(data_path_str)
+
+log_path_str = path_set(
+    '日志存放目录', 
+    'log_path', 
+    init_path=settings.game_log_path
+)
+
+input_path(log_path_str)
+
+cache_path_str = path_set(
+    '缓存存放目录', 
+    'cache_path', 
+    init_path=settings.cache_path
+)
+
+input_path(cache_path_str)
